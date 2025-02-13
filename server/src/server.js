99e8af1cd73fd601386969abf0ce6ee1a9743cfb
Mads Pedersen <mads.msc@gmail.com>
2025-02-13 15:17:03 +0100

    add server login

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,60 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const db = require('./db.json');
 
+const login = (message, ws) => {
+  const user = message.user;
+  const pass = message.pass;
+  if (!user || !pass) {
+    ws.send('{"msg":"invalid user or pass"}');
+    return;
+  }
+  const userFound = db.find(u => u.user === user && u.pass === pass);
+  if (userFound) {
+    const response = {
+      action: message.action,
+      player: userFound.player,
+      spaceship: userFound.spaceship
+    };
+    ws.send(JSON.stringify(response));
+  } else {
+    ws.send('{"msg":"no such user/pass combo"}');
+  }
+};
+
+const register = (message, ws) => {
+  console.log(`Received register message: ${message}`);
+  ws.send('{"msg":"not implemented"}');
+};
+
+const respond = (message, ws) => {
+  if (message?.action === 'login') {
+    login(message, ws);
+  } else if (message?.action === 'register') {
+    register(message, ws);
+  }
+};
+
+const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', (ws) => {
-  console.log('New client connected');
+  console.log('');
+  console.log('* New client connected');
 
   // Sending a message to the client
-  ws.send('Welcome to the WebSocket server!');
+  ws.send('{"msg":"Welcome to the WebSocket server!"}');
 
   // Listening for messages from the client
   ws.on('message', (message) => {
-    console.log(`Received message: ${message}`);
-    // Echoing the message back to the client
-    ws.send(`Server received: ${message}`);
+    const data = JSON.parse(message);
+    console.log('* Received message:', data);
+    respond(data, ws);
   });
 
   // Handling client disconnection
   ws.on('close', () => {
-    console.log('Client disconnected');
+    console.log('* Client disconnected');
   });
 });
 
-console.log('WebSocket server is running on ws://localhost:8080');
+console.log('* WebSocket server is running on ws://localhost:8080');
+console.log('');
